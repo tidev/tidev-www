80407e5511eb5a0a91fb3bc9a9e024d2c151a156
Sebastian Klaus <sebastian@lightapps.de>
2022-05-21 08:27:38 +0200

    open social links in new tab, cleaned up code

--- components/home/companies.tsx
+++ components/home/companies.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Companies() {
     return (
-        <div>
+        <>
             <div>Companies using Titanium? Sponsoring TiDev?</div>
-        </div>
+        </>
     );
 }
