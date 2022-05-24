d2cc69599f2c00352eff9997cfe65cf3987045b6
Sebastian Klaus <sebastian@lightapps.de>
2022-05-24 20:29:40 +0200

    feat: dark mode and mobile fixes

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -12,7 +12,7 @@ export default function Blog(props: InferGetStaticPropsType<typeof getStaticProp
             <div className='grid grid-cols-12 pb-10 sm:px-5 gap-x-8 gap-y-16'>
                 {props.pages.map((page: PageMeta, i: number) => (
                     <div key={i} className='flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4'>
-                        <div className='bg-blue-500 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white'>
+                        <div className='bg-blue-500 dark:bg-blue-700 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white'>
                             <span>{page.category}</span>
                         </div>
                         <h2 className='text-lg font-bold sm:text-xl md:text-2xl'>
