b4d08ab2f219f44ed35feb57a93ad88098e78e71
Chris Barber <chris@cb1inc.com>
2023-05-20 01:33:50 -0500

    Wired up signature pad, updated deps, lots of styling

--- pages/contribute.tsx
+++ pages/contribute.tsx
@@ -7,8 +7,16 @@ import type { CLAInfo } from '../utils/cla';
 import type { Session } from 'next-auth';
 import type { ExtendedProfile } from './api/auth/[...nextauth]';
 import SignatureCanvas from 'react-signature-canvas';
+import { Hurricane } from 'next/font/google';
+import localFont from 'next/font/local';
+import ReactSignatureCanvas from 'react-signature-canvas';
 
 const CONTRIBUTOR_PDF = 'CONTRIBUTOR_CLA_1.2.pdf';
+
+const signatureFont = Hurricane({
+	subsets: ['latin'],
+	weight: ['400']
+});
 
 pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.js';
 
@@ -189,7 +197,7 @@ function ShowCLAForm({ user }: { user: ExtendedProfile | null }) {
 			}
 			setPDFPages(pdfPages);
 		})();
-	}, []);
+	}, [user]);
 
 	return (
 		<>
@@ -224,6 +232,8 @@ function PDFPage({ page, user }: PDFPageParams) {
 		company: user?.company || '',
 		email: user?.email || ''
 	});
+	const [showModal, setShowModal] = useState(true);
+	const [sigText, setSigText] = useState('');
 
 	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setFormData(prev => ({
@@ -296,55 +306,62 @@ function PDFPage({ page, user }: PDFPageParams) {
 	const isEmailValid = () => /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(formData.email.trim());
 
 	const isValid = () => {
+		// TODO: check if signed!
 		return isFullNameValid() && isEmailValid();
-	};
-
-	const showSignatureModal = () => {
-		//
 	};
 
 	return (
 		<div className='relative'>
 			{user &&
-				<form action="/api/cla/sign" className="cla-form" method="post" ref={formRef}>
-					<div
-						className="cla-signature"
-						style={{ top: '240px' }}>
-						<span onClick={showSignatureModal}>
-							<span className="pr-2">Click to Sign</span>
-							<svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 24 24'>
-								<path d="M16.4798956,21.0019578 L16.75,21 C17.9702352,21 18.6112441,21.5058032 19.4020627,22.7041662 L19.7958278,23.3124409 C20.1028266,23.766938 20.2944374,23.9573247 20.535784,24.0567929 C20.9684873,24.2351266 21.3271008,24.1474446 22.6440782,23.5133213 L23.0473273,23.3170319 C23.8709982,22.9126711 24.4330286,22.6811606 25.0680983,22.5223931 C25.4699445,22.4219316 25.8771453,22.6662521 25.9776069,23.0680983 C26.0780684,23.4699445 25.8337479,23.8771453 25.4319017,23.9776069 C25.0371606,24.0762922 24.6589465,24.2178819 24.1641364,24.4458997 L23.0054899,25.0032673 C21.4376302,25.7436944 20.9059009,25.8317321 19.964216,25.4436275 C19.3391237,25.1860028 18.9836765,24.813298 18.4635639,24.0180227 L18.2688903,23.7140849 C17.6669841,22.7656437 17.3640608,22.5 16.75,22.5 L16.5912946,22.5037584 C16.1581568,22.5299816 15.8777212,22.7284469 14.009281,24.1150241 C12.2670395,25.4079488 10.9383359,26.0254984 9.24864243,26.0254984 C7.18872869,26.0254984 5.24773367,25.647067 3.43145875,24.8905363 L6.31377803,24.2241784 C7.25769404,24.4250762 8.23567143,24.5254984 9.24864243,24.5254984 C10.5393035,24.5254984 11.609129,24.0282691 13.1153796,22.9104743 L14.275444,22.0545488 C15.5468065,21.1304903 15.8296113,21.016032 16.4798956,21.0019578 L16.4798956,21.0019578 Z M22.7770988,3.22208979 C24.4507223,4.8957133 24.4507566,7.60916079 22.7771889,9.28281324 L21.741655,10.3184475 C22.8936263,11.7199657 22.8521526,13.2053774 21.7811031,14.279556 L18.7800727,17.2805874 L18.7800727,17.2805874 C18.4870374,17.5733384 18.0121637,17.573108 17.7194126,17.2800727 C17.4266616,16.9870374 17.426892,16.5121637 17.7199273,16.2194126 L20.7188969,13.220444 C21.2039571,12.7339668 21.2600021,12.1299983 20.678941,11.3818945 L10.0845437,21.9761011 C9.78635459,22.2743053 9.41036117,22.482705 8.99944703,22.5775313 L2.91864463,23.9807934 C2.37859061,24.1054212 1.89457875,23.6214094 2.0192066,23.0813554 L3.42247794,17.0005129 C3.51729557,16.5896365 3.72566589,16.2136736 4.0238276,15.9154968 L16.7165019,3.22217992 C18.3900415,1.54855555 21.1034349,1.54851059 22.7770988,3.22208979 Z"></path>
-							</svg>
-						</span>
-					</div>
-					<input
-						name="fullname"
-						className={isFullNameValid() ? '' : 'cla-invalid'}
-						required
-						onChange={handleInput}
-						value={formData.fullname}
-						style={{ top: '365px' }}/>
-					<input
-						name="title"
-						onChange={handleInput}
-						value={formData.title}
-						style={{ top: '460px' }}/>
-					<input
-						name="company"
-						onChange={handleInput}
-						value={formData.company}
-						style={{ top: '554px' }}/>
-					<input
-						name="email"
-						className={isEmailValid() ? '' : 'cla-invalid'}
-						required
-						onChange={handleInput}
-						value={formData.email}
-						style={{ top: '650px' }}/>
-					<div className="cla-submit" style={{ top: '950px' }}>
-						<button className='button' type="submit" disabled={!isValid()}>Submit</button>
-					</div>
-				</form>
+				<>
+					<form action="/api/cla/sign" className="cla-form" method="post" ref={formRef}>
+						<div
+							className="cla-signature"
+							onClick={() => setShowModal(true)}
+							style={{ top: '240px' }}>
+							<span>
+								<span className="pr-2">Click to Sign</span>
+								<svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 24 24'>
+									<path d="M16.4798956,21.0019578 L16.75,21 C17.9702352,21 18.6112441,21.5058032 19.4020627,22.7041662 L19.7958278,23.3124409 C20.1028266,23.766938 20.2944374,23.9573247 20.535784,24.0567929 C20.9684873,24.2351266 21.3271008,24.1474446 22.6440782,23.5133213 L23.0473273,23.3170319 C23.8709982,22.9126711 24.4330286,22.6811606 25.0680983,22.5223931 C25.4699445,22.4219316 25.8771453,22.6662521 25.9776069,23.0680983 C26.0780684,23.4699445 25.8337479,23.8771453 25.4319017,23.9776069 C25.0371606,24.0762922 24.6589465,24.2178819 24.1641364,24.4458997 L23.0054899,25.0032673 C21.4376302,25.7436944 20.9059009,25.8317321 19.964216,25.4436275 C19.3391237,25.1860028 18.9836765,24.813298 18.4635639,24.0180227 L18.2688903,23.7140849 C17.6669841,22.7656437 17.3640608,22.5 16.75,22.5 L16.5912946,22.5037584 C16.1581568,22.5299816 15.8777212,22.7284469 14.009281,24.1150241 C12.2670395,25.4079488 10.9383359,26.0254984 9.24864243,26.0254984 C7.18872869,26.0254984 5.24773367,25.647067 3.43145875,24.8905363 L6.31377803,24.2241784 C7.25769404,24.4250762 8.23567143,24.5254984 9.24864243,24.5254984 C10.5393035,24.5254984 11.609129,24.0282691 13.1153796,22.9104743 L14.275444,22.0545488 C15.5468065,21.1304903 15.8296113,21.016032 16.4798956,21.0019578 L16.4798956,21.0019578 Z M22.7770988,3.22208979 C24.4507223,4.8957133 24.4507566,7.60916079 22.7771889,9.28281324 L21.741655,10.3184475 C22.8936263,11.7199657 22.8521526,13.2053774 21.7811031,14.279556 L18.7800727,17.2805874 L18.7800727,17.2805874 C18.4870374,17.5733384 18.0121637,17.573108 17.7194126,17.2800727 C17.4266616,16.9870374 17.426892,16.5121637 17.7199273,16.2194126 L20.7188969,13.220444 C21.2039571,12.7339668 21.2600021,12.1299983 20.678941,11.3818945 L10.0845437,21.9761011 C9.78635459,22.2743053 9.41036117,22.482705 8.99944703,22.5775313 L2.91864463,23.9807934 C2.37859061,24.1054212 1.89457875,23.6214094 2.0192066,23.0813554 L3.42247794,17.0005129 C3.51729557,16.5896365 3.72566589,16.2136736 4.0238276,15.9154968 L16.7165019,3.22217992 C18.3900415,1.54855555 21.1034349,1.54851059 22.7770988,3.22208979 Z"></path>
+								</svg>
+							</span>
+						</div>
+						<input
+							name="fullname"
+							className={isFullNameValid() ? '' : 'cla-invalid'}
+							required
+							onChange={handleInput}
+							value={formData.fullname}
+							style={{ top: '365px' }}/>
+						<input
+							name="title"
+							onChange={handleInput}
+							value={formData.title}
+							style={{ top: '460px' }}/>
+						<input
+							name="company"
+							onChange={handleInput}
+							value={formData.company}
+							style={{ top: '554px' }}/>
+						<input
+							name="email"
+							className={isEmailValid() ? '' : 'cla-invalid'}
+							required
+							onChange={handleInput}
+							value={formData.email}
+							style={{ top: '650px' }}/>
+						<div className="cla-submit" style={{ top: '950px' }}>
+							<button className='button' type="submit" disabled={!isValid()}>Submit</button>
+						</div>
+					</form>
+					{showModal &&
+						<SignatureModal
+							fullname={sigText || formData.fullname}
+							setShowModal={setShowModal}
+							onSaveSigText={setSigText}
+							/>
+					}
+				</>
 			}
 			{user === null && <>
 				<div className='cla-login'>
@@ -358,5 +375,105 @@ function PDFPage({ page, user }: PDFPageParams) {
 			</>}
 			<canvas id={user ? 'sign' : undefined} ref={canvasRef} style={{ border: '1px solid black', direction: 'ltr' }} />
 		</div>
+	);
+}
+
+function SignatureModal({
+	fullname,
+	onSaveSigText,
+	setShowModal
+}: {
+	fullname: string;
+	onSaveSigText: (sigText: string) => void;
+	setShowModal: (showModal: boolean) => void;
+}) {
+	const [activeTab, setActiveTab] = useState('sign');
+	const [sigText, setSigText] = useState(fullname);
+	const sigCanvasRef = useRef<ReactSignatureCanvas | null>(null);
+	const sigTextRef = useRef<HTMLInputElement | null>(null);
+
+	const onSign = () => {
+		setShowModal(false);
+		onSaveSigText(sigText);
+	};
+
+	useEffect(() => {
+		if (activeTab === 'type') {
+			sigTextRef.current?.focus();
+		}
+	}, [activeTab, sigTextRef]);
+
+	const reset = () => {
+		sigCanvasRef.current?.clear();
+	};
+
+	useEffect(() => {
+		const sigCanvas = sigCanvasRef.current?.getCanvas();
+		if (activeTab === 'sign' && sigCanvas?.parentElement) {
+			const w = parseInt(window.getComputedStyle(sigCanvas.parentElement).width);
+			if (sigCanvas.width !== w) {
+				sigCanvas.width = w;
+			}
+		}
+	}, [activeTab, sigCanvasRef]);
+
+	return (
+		<>
+			<div className="cla-modal">
+				<div className="cla-modal-squeeze">
+					<div className="cla-modal-win">
+						<div className="tab-bar">
+							<button
+								className={activeTab === 'sign' ? 'active' : ''}
+								onClick={() => setActiveTab('sign')}>Sign</button>
+							<button
+								className={activeTab === 'type' ? 'active' : ''}
+								onClick={() => setActiveTab('type')}>Type</button>
+						</div>
+						<div className="relative px-6 py-2 flex-auto">
+							<div className={`cla-sig-bg ${activeTab === 'sign' ? 'block' : 'hidden'}`}>
+								<SignatureCanvas
+									canvasProps={{
+										height: 120
+									}}
+									clearOnResize={false}
+									ref={sigCanvasRef}
+									/>
+							</div>
+							<div className={`cla-sig-bg cla-sig-bg-line ${activeTab === 'type' ? 'block' : 'hidden'}`}>
+								<input
+									autoComplete="off"
+									className={`cla-sign-type ${signatureFont.className}`}
+									name="sig-text"
+									onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSigText(e.target.value)}
+									ref={sigTextRef}
+									type="text"
+									value={sigText}
+									/>
+							</div>
+						</div>
+						<div className="flex items-center p-6 rounded-b">
+							{activeTab === 'sign' && <button
+								className="button-secondary"
+								type="button"
+								onClick={reset}
+								>Reset</button>
+							}
+							<button
+								className="button-secondary mr-12 ml-auto"
+								type="button"
+								onClick={() => setShowModal(false)}
+								>Cancel</button>
+							<button
+								className="button"
+								type="button"
+								onClick={onSign}
+								>Save</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="cla-modal-bg"></div>
+		</>
 	);
 }
