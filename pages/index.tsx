2b048fbdc4b4b57479d2b364478785bff36f1b0d
Chris Barber <chris@cb1inc.com>
2023-09-16 23:45:52 -0500

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-// import Head from 'next/head';
-// import Image from 'next/image';
 import Hero from '../components/home/hero';
 import About from '../components/home/about';
 // import Team from '../components/home/team';
